341257899d886ad10ed1562ab9bd71ab89847b66
Christopher Burr <chrisburr73@googlemail.com>
2015-12-26 00:43:14 +0000

    Remove depreciated customization_script.sh and minor fixes to setup_cloudinit.sh

--- scripts/during_chroot/setup_cloudinit.sh
+++ scripts/during_chroot/setup_cloudinit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pacman -Syy
-pacman --noconfirm -S wget zsh
+pacman --noconfirm -S wget zsh git
 
 wget https://gist.githubusercontent.com/chrisburr/3e3a5f34def0a5a9f5d7/raw/2cd497f966f4855628e9158fa6bdc11cf58a153d/cloud.cfg
-chown --reference=/etc/cloud/cloud.cfg cloud.cf
-chmod --reference=/etc/cloud/cloud.cfg cloud.cf
+chown --reference=/etc/cloud/cloud.cfg cloud.cfg
+chmod --reference=/etc/cloud/cloud.cfg cloud.cfg
 mv cloud.cfg /etc/cloud/cloud.cfg
